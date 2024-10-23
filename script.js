d6e8cde6946188f52ca46b8b2d4185446ccd8b8e
Nimesh5090 <nimeshbharai5090@gmail.com>
2024-10-23 20:13:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let div1 = document.getElementById("div1");
+div1.innerText= "Hello I am a Div!"
+div1.style.margin = "20px"
+div1.style.backgroundColor = "yellow"
+div1.style.padding = "10px"
+div1.style.fontSize = "18px"
+div1.style.fontWeight = "bold"
+div1.style.height = "300px"
+div1.style.weight = "200px"
+console.log(div1);
+
+
+
+
+
